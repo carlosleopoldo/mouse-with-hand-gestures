6fca76fc063df3b02ae69c58cafdf347f61f29ac
Carlos Leopoldo <carlos@carlosleopoldo.com>
2024-09-20 11:33:28 -0600

    Update util.py

--- util.py
+++ util.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+
 def get_angle(a, b, c):
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
+        a[1] - b[1], a[0] - b[0]
+    )
     angle = np.abs(np.degrees(radians))
     return angle
 
